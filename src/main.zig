cb5b6b8fbc526f704853cf15dbc0d9b7b6689372
Paul Hariel <paul.hariel@outlook.com>
2024-04-01 19:05:19 +0200

    Move cpu functions to driver file

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,10 @@
+const cpuDriver = @import("drivers/system/cpu.zig");
+
 /// This function is the entry point of the kernel.
 /// It is called by the bootloader after the kernel is loaded into memory.
 /// It is marked as `noreturn` because it should never return.
 export fn _start() callconv(.C) noreturn {
-    halt();
-}
-
-pub fn halt() noreturn {
-    while (true) {
-        asm volatile ("hlt");
-    }
+    const cpu = cpuDriver.Cpu;
+    cpu.disable_interrupts();
+    cpu.halt();
 }
