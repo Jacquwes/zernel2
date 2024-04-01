16d58a55e4cd61b37b320ab39713bd5ff451061d
Paul Hariel <paul.hariel@outlook.com>
2024-04-01 19:35:31 +0200

    Don't use intermediate variable

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,9 @@
-const cpuDriver = @import("drivers/system/cpu.zig");
+const Cpu = @import("drivers/system/cpu.zig").Cpu;
 
 /// This function is the entry point of the kernel.
 /// It is called by the bootloader after the kernel is loaded into memory.
 /// It is marked as `noreturn` because it should never return.
 export fn _start() callconv(.C) noreturn {
-    const cpu = cpuDriver.Cpu;
-    cpu.disable_interrupts();
-    cpu.halt();
+    Cpu.disable_interrupts();
+    Cpu.halt();
 }
