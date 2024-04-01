27ccfe0a16d33f5007475c1c38054e5a266b1e4c
Paul Hariel <paul.hariel@outlook.com>
2024-04-01 19:35:57 +0200

    Init pic

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,12 @@
 const Cpu = @import("drivers/system/cpu.zig").Cpu;
+const Pic = @import("drivers/system/pic.zig").Pic;
 
 /// This function is the entry point of the kernel.
 /// It is called by the bootloader after the kernel is loaded into memory.
 /// It is marked as `noreturn` because it should never return.
 export fn _start() callconv(.C) noreturn {
+    // Initialize the PIC
+    Pic.remap();
     Cpu.disable_interrupts();
     Cpu.halt();
 }
