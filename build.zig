16c4118d214fb5f042da38887059b2080ee4ab6e
Paul Hariel <paul.hariel@outlook.com>
2024-04-04 17:40:31 +0200

    Add steps to build and run the iso

--- build.zig
+++ build.zig
@@ -25,4 +25,18 @@ pub fn build(b: *std.Build) !void {
     kernel.pie = false;
 
     b.installArtifact(kernel);
+
+    var iso_step = b.step("iso", "build ISO file for Zernel 2");
+    const iso_command = b.addSystemCommand(&[_][]const u8{"./commands/build_iso.sh"});
+    iso_command.step.dependOn(&kernel.step);
+    iso_step.dependOn(&iso_command.step);
+
+    var run_step = b.step("run", "run Zernel 2 in QEMU");
+    const run_command = b.addSystemCommand(&[_][]const u8{"./commands/run.sh"});
+    run_command.step.dependOn(iso_step);
+    run_step.dependOn(&run_command.step);
+
+    var debug_step = b.step("debug", "debug Zernel 2 in GDB");
+    const debug_command = b.addSystemCommand(&[_][]const u8{"./commands/debug.sh"});
+    debug_step.dependOn(&debug_command.step);
 }
