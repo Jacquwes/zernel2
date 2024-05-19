e424e3c0d946a2c0e46baf1934d8a2b4c6c3c8d7
Paul Hariel <paul.hariel@outlook.com>
2024-05-19 19:22:02 +0200

    Log interrupts

--- commands/run.sh
+++ commands/run.sh
@@ -4,5 +4,6 @@ qemu-system-x86_64 \
     -drive format=raw,file=zernel2.iso \
     -serial stdio \
     -s\
-    -D qemu.log -d cpu_reset \
-    -drive format=raw,file=hd.img
+    -D qemu.log -d cpu_reset,int \
+    -drive format=raw,file=hd.img \
+    -no-reboot
