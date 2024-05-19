ede9a57b9e8fb8e29c35170b7685c7a993fc46c1
Paul Hariel <paul.hariel@outlook.com>
2024-05-19 17:10:03 +0200

    Add drive

--- commands/run.sh
+++ commands/run.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-qemu-system-x86_64 zernel2.iso -serial stdio -s -S
+qemu-system-x86_64 \
+    -drive format=raw,file=zernel2.iso \
+    -serial stdio \
+    -s\
+    -D qemu.log -d cpu_reset \
+    -drive format=raw,file=hd.img
